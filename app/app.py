2e6e6f4429e506c2c7ab007e809ad1af4f47f787
aniket36565954961 <aniketspawar3031@gmail.com>
2026-03-14 20:22:46 +0530

    Updated application message

--- app/app.py
+++ app/app.py
@@ -1,8 +1,10 @@
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "CI/CD Pipeline Demo Running 🚀"
+    return "CI/CD Pipeline Demo Running"
 
-app.run(host="0.0.0.0", port=5000
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
